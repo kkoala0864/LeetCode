61f3dda84b0ea2b8ad7ab1fb414b4bce703fb73b
neilhuang <neilhuang@synology.com>
2017-06-07 21:02:12 +0800

    <LeetCode> Clean template

--- 0000_Template/Solution.h
+++ 0000_Template/Solution.h
@@ -5,7 +5,6 @@ class Solution {
     public :
         Solution() {}
         std::vector<int> twoSum(std::vector<int>& nums, int target);
-        void DebugPrint(std::vector<int> nums);
     private :
         virtual ~Solution() {}
         Solution& operator=(const Solution& source);
--- 0000_Template/main.cpp
+++ 0000_Template/main.cpp
@@ -4,14 +4,6 @@ using namespace std;
 int main(void) {
     Solution* test = new Solution();
 
-    vector<int> input;
-    input.push_back(3);
-    input.push_back(2);
-    input.push_back(4);
-    test->DebugPrint(input);
-    test->DebugPrint(test->twoSum(input, 6));
-
-
     return 0;
 }
 
