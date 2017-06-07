85f00ee7e515091db50f42ef38a38080e800f080
neilhuang <neilhuang@synology.com>
2017-06-07 20:58:13 +0800

    <LeetCode> 0500 Keyboard Row Refactor

--- 0500_Keyboard_Row/Solution.cpp
+++ 0500_Keyboard_Row/Solution.cpp
@@ -10,41 +10,24 @@ vector<string> Solution::findWords(vector<string>& words) {
     vector<string> ret;
     unordered_map<int,int> hash_table;
 
-    hash_table['q']=1;
-    hash_table['w']=1;
-    hash_table['e']=1;
-    hash_table['r']=1;
-    hash_table['t']=1;
-    hash_table['y']=1;
-    hash_table['u']=1;
-    hash_table['i']=1;
-    hash_table['o']=1;
-    hash_table['p']=1;
-    hash_table['a']=2;
-    hash_table['s']=2;
-    hash_table['d']=2;
-    hash_table['f']=2;
-    hash_table['g']=2;
-    hash_table['h']=2;
-    hash_table['j']=2;
-    hash_table['k']=2;
-    hash_table['l']=2;
-    hash_table['z']=3;
-    hash_table['x']=3;
-    hash_table['c']=3;
-    hash_table['v']=3;
-    hash_table['b']=3;
-    hash_table['n']=3;
-    hash_table['m']=3;
+    string one("qwertyuiop");
+    string two("asdfghjkl");
+    string three("zxcvbnm");
+    for ( const auto& oneIdx : one )
+	hash_table[static_cast<int>(oneIdx)] = 1;
+    for ( const auto& twoIdx : two )
+	hash_table[static_cast<int>(twoIdx)] = 2;
+    for ( const auto& threeIdx : three )
+	hash_table[static_cast<int>(threeIdx)] = 3;
 
     for (size_t wIdx=0 ; wIdx < words.size() ; ++wIdx) {
 	int change(0);
 	string compString = words[wIdx];
         transform(words[wIdx].begin(), words[wIdx].end(), compString.begin(), ::tolower);
-        int curStat = hash_table[(int)(compString.at(0))];
+        int curStat = hash_table[static_cast<int>(compString.at(0))];
 
         for (size_t sIdx=1 ; sIdx < compString.size() ; ++sIdx) {
-            if (curStat != hash_table[(int)(compString.at(sIdx))]) {
+            if (curStat != hash_table[static_cast<int>(compString.at(sIdx))]) {
 		change = 1;
 		break;
             }
