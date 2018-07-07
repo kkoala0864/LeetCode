76745595b6afcd57697d05b4a5966707e9c9bfda
neilhuang <neilhuang@synology.com>
2018-07-07 15:53:25 +0800

    <LeetCode> 0235 LCA Of BST update

--- 0235_LCA_Of_BST/Solution.cpp
+++ 0235_LCA_Of_BST/Solution.cpp
@@ -1,27 +1,19 @@
 #include <Solution.h>
 
 TreeNode* Solution::lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-	if (root == nullptr) {
-		return nullptr;
-	}
+    if (root == nullptr) return nullptr;
 
-	if (nullptr == p || nullptr == q) {
-		return root;
+    TreeNode* iter = root;
+    TreeNode* big = p->val >= q->val ? p : q;
+    TreeNode* small = big == p ? q : p;
+    while (iter) {
+	if ((iter == big) || (iter == small)) return iter;
+	if ((iter->val > small->val) && (iter->val < big->val)) return iter;
+	if (iter->val > big->val) {
+	    iter = iter->left;
+	} else if (iter->val < small->val) {
+	    iter = iter->right;
 	}
-
-	TreeNode *iter = root;
-	TreeNode *big = p->val > q->val ? p : q;
-	TreeNode *small = big == p ? q : p;
-
-	while (iter) {
-		if (iter->val > big->val) {
-			iter = iter->left;
-		} else {
-			if (iter->val < small->val) {
-				iter = iter->right;
-			} else {
-				return iter;
-			}
-		}
-	}
+    }
+    return nullptr;
 }
