fed73fd27d4a63662772a603dba8123dbc5bc78c
neilhuang <neilhuang@synology.com>
2018-07-08 15:07:50 +0800

    <LeetCode> 0319 Bulb Switcher

---
+++ 0319_Bulb_Switcher/main.cpp
@@ -0,0 +1,10 @@
+#include <Solution.h>
+#include <iostream>
+using namespace std;
+int main(void) {
+    Solution* test = new Solution();
+    cout << test->bulbSwitch(99) << endl;
+
+    return 0;
+}
+
