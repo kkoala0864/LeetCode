8954ccc424b49e71ddc518a642c15aaf2116c5e8
neilhuang <neilhuang@synology.com>
2021-04-10 13:00:52 +0800

    <LeetCode> 322. Coin Change

--- 0322_Coin_Change/Solution.cpp
+++ 0322_Coin_Change/Solution.cpp
@@ -32,15 +32,12 @@ int Solution::coinChangeRecur(vector<int>& coins, int amount) {
 
 int Solution::coinChange(vector<int>& coins, int amount) {
 	vector<int> dp(amount + 1, INT_MAX);
-	int size = coins.size();
-	dp.at(0) = 0;
-	for (int i = 1 ; i <= amount ; ++i) {
-		for (int j = 0 ; j < size ; ++j) {
-			if (i - coins.at(j) >= 0) {
-				if (dp.at(i - coins.at(j)) != INT_MAX)
-					dp.at(i) = min(dp.at(i), dp.at(i - coins.at(j)) + 1);
-			}
+	dp[0] = 0;
+	for (const auto& coin: coins) {
+		for (int i = coin ; i <= amount ; ++i) {
+			if (dp[i - coin] != INT_MAX)
+				dp[i] = min(dp[i], dp[i - coin] + 1);
 		}
 	}
-	return dp.at(amount) == INT_MAX ? -1 : dp.at(amount);
+	return dp[amount] == INT_MAX ? -1 : dp[amount];
 }
--- 0322_Coin_Change/main.cpp
+++ 0322_Coin_Change/main.cpp
@@ -6,8 +6,8 @@ using std::endl;
 
 int main(void) {
     Solution* test = new Solution();
-    vector<int> coins = {2};
-    cout << test->coinChange(coins, 3) << endl;
+    vector<int> coins = {1,2,5};
+    cout << test->coinChange(coins, 11) << endl;
     return 0;
 }
 
